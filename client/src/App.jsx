bf1db1bb4051dc8ec2d97b1a9d2c8564c46140e3
Kevin J. Zea <kevinjzea@gmail.com>
2023-10-20 02:55:19 -0600

    ➕ feat: Implementing components and creating  beta UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,67 @@
-import './App.css';
+import { useState } from 'react';
+import { Home } from './pages/Home';
+import { ChatButton } from './components/ChatButton';
+import { ChatPortal } from './components/ChatPortal';
+import { LoginForm } from './components/LoginForm';
+import { Message } from './components/Message';
+import './App.scss';
 
 function App() {
+  const [userData, setUserData] = useState({ username: '' });
+  const [sessionData, setSessionData] = useState({
+    darkMode: true,
+    isChatOpen: false,
+    messages: [],
+  });
+
+  const toggleChat = () => {
+    setSessionData((prevSessionData) => ({
+      ...prevSessionData,
+      isChatOpen: !prevSessionData.isChatOpen,
+    }));
+  };
+
+  const closeChat = () => {
+    setSessionData((prevSessionData) => ({
+      ...prevSessionData,
+      isChatOpen: false,
+    }));
+  };
+
+  const addMessage = (content) => {
+    setSessionData((prevSessionData) => ({
+      ...prevSessionData,
+      messages: [
+        ...prevSessionData.messages,
+        { sender: userData.username, content },
+      ],
+    }));
+  };
+
   return (
     <>
-      <h1>
-        TicTacToe made by{' '}
-        <a href="https://github.com/kevinjzea">Kevin J. Zea</a> coming soon!
-      </h1>
+      {userData.username.length === 0 ? (
+        <LoginForm setUserData={setUserData} />
+      ) : (
+        <Home>
+          <ChatButton onClick={toggleChat} />
+          {sessionData.isChatOpen ? (
+            <ChatPortal
+              closeChat={closeChat}
+              addMessage={addMessage}
+            >
+              {sessionData.messages.map((message) => (
+                <Message
+                  key={message.id}
+                  content={message.content}
+                  sender={message.sender}
+                  isSameUser={message.sender === userData.username}
+                />
+              ))}
+            </ChatPortal>
+          ) : null}
+        </Home>
+      )}
     </>
   );
 }
