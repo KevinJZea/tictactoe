b566f9761945c86cbd48baff6df68e08549bde4f
Kevin J. Zea <kevinjzea@gmail.com>
2023-10-23 22:08:38 -0600

    ➕ feat: Text that shows current  turn created and displayed

--- client/src/components/ScoreBoard/ScoreBoard.jsx
+++ client/src/components/ScoreBoard/ScoreBoard.jsx
@@ -3,18 +3,25 @@ import './ScoreBoard.scss';
 
 export function ScoreBoard() {
   const { state } = useAppContext();
-  const { rival, user } = state;
+  const { rival, turn, user } = state;
 
   return (
-    <div className="ScoreBoard">
-      <div className="ScoreBoard--info-container">
-        <span className="ScoreBoard--username">{user.username}</span>
-        <span className="ScoreBoard--points">{user.points}</span>
+    <div className="ScoreBoard--main-container">
+      <div className="ScoreBoard">
+        <div className="ScoreBoard--info-container">
+          <span className="ScoreBoard--username">{user.username}</span>
+          <span className="ScoreBoard--points">{user.points}</span>
+        </div>
+        <div className="ScoreBoard--info-container">
+          <span className="ScoreBoard--username">
+            {rival.username || 'CPU'}
+          </span>
+          <span className="ScoreBoard--points">{rival.points}</span>
+        </div>
       </div>
-      <div className="ScoreBoard--info-container">
-        <span className="ScoreBoard--username">{rival.username || 'CPU'}</span>
-        <span className="ScoreBoard--points">{rival.points}</span>
-      </div>
+      <h3 className="ScoreBoard--turn-text">
+        {rival.mark === turn ? `${rival.username}'s` : 'Your'} turn
+      </h3>
     </div>
   );
 }
