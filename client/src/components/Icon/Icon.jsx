f71a47c90bc625309073ba5509ea5f8934dbc36c
Kevin J. Zea <kevinjzea@gmail.com>
2023-10-26 20:42:37 -0600

    ➕ feat: Dark/Light mode added

--- client/src/components/Icon/Icon.jsx
+++ client/src/components/Icon/Icon.jsx
@@ -1,4 +1,6 @@
 import MessageCircle from '../../assets/svg/message-circle.svg?react';
+import Moon from '../../assets/svg/moon.svg?react';
+import Sun from '../../assets/svg/sun.svg?react';
 import X from '../../assets/svg/x.svg?react';
 
 import { ICONS } from '../../utils/constants';
@@ -7,6 +9,8 @@ import './Icon.scss';
 
 const icons = {
   [ICONS.MessageCircle]: <MessageCircle />,
+  [ICONS.Moon]: <Moon />,
+  [ICONS.Sun]: <Sun />,
   [ICONS.X]: <X />,
 };
 
